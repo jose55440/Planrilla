20c77a813935e278c7ba7ac5606af70e491c37bf
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2023-12-27 23:54:42 +0100

    27/12/2023

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,21 @@
-const { readFileSync } = require('fs')
- 
-const posthtml = require('posthtml')
-const include = require('posthtml-include')
- 
-const html = readFileSync('index.html')
- 
-posthtml([ include({ encoding: 'utf8' }) ])
-    .process(html)
-    .then((result) => console.log(result.html))
-    
+// Atajos
+const { readFileSync } = require("fs");
+
+const posthtml = require("posthtml");
+const include = require("posthtml-include");
+
+const html = readFileSync("index.html");
+
+posthtml([include({ encoding: "utf8" })])
+  .process(html)
+  .then((result) => console.log(result.html));
+
+  const swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 2000, // Esto también configura el tiempo de reproducción automática
+    },
+  });
