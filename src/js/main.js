3e1afb1ffea9df67860b92379a9c17de19b57218
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2023-12-27 17:29:47 +0100

    27/12/2023

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log("hola buenas esto es de prueba");
+const { readFileSync } = require('fs')
+ 
+const posthtml = require('posthtml')
+const include = require('posthtml-include')
+ 
+const html = readFileSync('index.html')
+ 
+posthtml([ include({ encoding: 'utf8' }) ])
+    .process(html)
+    .then((result) => console.log(result.html))
+    
