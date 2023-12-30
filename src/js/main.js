78d373ac6885655d878bd61c6b65829c145f595f
jose55440 <josemanuelgarciasanchez10@yahoo.com>
2023-12-30 15:43:24 +0100

    30/12/2023

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,34 @@
-const posthtml = require('posthtml');
-const include = require('posthtml-include');
-const Swiper = require('swiper/swiper-bundle.min.js');
+const posthtml = require("posthtml");
+const include = require("posthtml-include");
+const Swiper = require("swiper/swiper-bundle.min.js");
 
 // Assuming index.html is in the same directory as your main.js
-const htmlPath = 'index.html';
+const htmlPath = "index.html";
 
 // Fetch the HTML content asynchronously
 fetch(htmlPath)
   .then((response) => response.text())
   .then((html) => {
     // Process HTML using posthtml and posthtml-include
-    return posthtml([include({ encoding: 'utf8' })])
-      .process(html);
-  })
-  
-
-
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-
-  const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
-    disabledClass:'swiper-button-disabled',
-    enabled: true,
-    hiddenClass:'swiper-button-hidden',
-    hideOnClick:false,
-    lockClass:	'swiper-button-lock',
-    navigationDisabledClass:'swiper-navigation-disabled',
-    nextEl:null,
-    prevEl:null
+    return posthtml([include({ encoding: "utf8" })]).process(html);
   });
 
-    
-    swiper.slideNext();
-    swiper.slideNext();
-    
-  
-  
- 
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
-  
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  spaceBetween: 100,
+  disabledClass: "swiper-button-disabled",
+  enabled: true,
+  hiddenClass: "swiper-button-hidden",
+  hideOnClick: false,
+  lockClass: "swiper-button-lock",
+  navigationDisabledClass: "swiper-navigation-disabled",
+});
+document.querySelector("#next").addEventListener("click", function () {
+  swiper.slideNext();
+});
+document.querySelector("#prev").addEventListener("click", function () {
+  swiper.slidePrev();
+});
